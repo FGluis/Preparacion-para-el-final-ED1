8bad379d19efbea5440fc0b43a7e11b39abc378f
Jesus Zuluaga Moreno <citozuluaga44@gmail.com>
2022-11-19 19:52:04 +0000

    Guardado temporal

--- SupermercadoMultilista/package/classes.py
+++ SupermercadoMultilista/package/classes.py
@@ -6,12 +6,11 @@ class Box:
         self.estado = estado 
         self.link1 = None 
         self.clientes = ListaClientes()
-        self.n_cliente = self.clientes.n_cliente
-        
-    def __str__(self) -> str:
-        return f'|| Nro clientes: {self.n_cliente} - Monto: {self.monto} ||'
 
-class Cliente:
+    def __str__(self) -> str:
+        return f'|| Nro clientes: {self.clientes.n_cliente} - Monto: {self.monto} ||'
+
+class Client:
     def __init__(self, id_cliente: int, monto_cliente: int) -> None:
         self.id_cliente = id_cliente
         self.monto_cliente = monto_cliente
@@ -40,7 +39,7 @@ class ListaClientes:
             current = current.link2
         return tam
     
-    def addCliente(self, cliente: Cliente):
+    def addClient(self, cliente: Client):
         p = cliente
         if self.head == None:
             self.head = p 
@@ -51,7 +50,25 @@ class ListaClientes:
         self.tail.link2 = p
         self.tail = self.tail.link2
         self.n_cliente += 1
-    
+
+    def deleteClient(self, client: Client):
+        p = client 
+        current = self.head 
+        prev = None 
+
+        while current != None and current != client:
+            prev = current 
+            current = current.link2
+
+        if client == self.head:
+            self.head = current.link2
+        if client == self.tail:
+            self.tail = prev 
+            prev.link2 = None 
+        else:
+            prev.link2 = current.link2 
+            current.link2 = None
+
     def __str__(self) -> str:
         return ' -> '.join([str(cliente) for cliente in self])
 
@@ -79,8 +96,16 @@ class ListaCajas:
     def __str__(self) -> None:
         return ' -> '.join([str(box) for box in self])
 
-cliente1 = Cliente(id_cliente=10, monto_cliente=2000)
-cliente2 = Cliente(id_cliente=12, monto_cliente=3000)
+cliente1 = Client(id_cliente=10, monto_cliente=2000)
+cliente2 = Client(id_cliente=12, monto_cliente=3000)
+cliente3 = Client(id_cliente=9, monto_cliente=2000)
+cliente4 = Client(id_cliente=8, monto_cliente=3000)
+cliente5 = Client(id_cliente=77, monto_cliente=2000)
+cliente6 = Client(id_cliente=1, monto_cliente=2000)
+cliente7 = Client(id_cliente=4, monto_cliente=3000)
+cliente8 = Client(id_cliente=5, monto_cliente=2000)
+cliente9 = Client(id_cliente=44, monto_cliente=3000)
+
 
 boxes = [Box(monto=10000, estado=False),
          Box(monto=5000, estado=False),
@@ -91,15 +116,23 @@ boxes = [Box(monto=10000, estado=False),
 lista_clientes = ListaClientes()
 lista_cajas = ListaCajas()
 
-lista_clientes.addCliente(cliente1)
-lista_clientes.addCliente(cliente2)
+lista_clientes.addClient(cliente3)
+lista_clientes.addClient(cliente4)
+lista_clientes.addClient(cliente1)
+lista_clientes.addClient(cliente2)
 
 for i in range(len(boxes)):
     lista_cajas.addBox(boxes[i])
 
-lista_cajas.head.clientes.addCliente(cliente1)
+lista_cajas.head.clientes.addClient(cliente1)
+lista_cajas.head.clientes.addClient(cliente2)
+lista_cajas.head.link1.clientes.addClient(cliente2)
 
-
+print('Before deleting')
+print(lista_clientes)
+# lista_clientes.deleteClient(cliente1)
+print('======================================')
+print('After deleting')
 print(lista_clientes)
 print('======================================')
 print(lista_cajas)
